02a0a53bf8a6268ecfbc9d545363b495ee60aad5
Chanadu <73862221+Chanadu@users.noreply.github.com>
2022-12-20 09:47:48 -0600

    Delete src/me/Chanadu directory

--- src/me/Chanadu/ArcadeSetup/Arcade.java
+++
@@ -1,7 +0,0 @@
-package me.Chanadu.ArcadeSetup;
-
-public class Arcade {
-	public static void main(String[] args) {
-		new ArcadeFrame();
-	}
-}
--- src/me/Chanadu/ArcadeSetup/CreditsScreen.java
+++
@@ -1,9 +0,0 @@
-package me.Chanadu.ArcadeSetup;
-
-import javax.swing.*;
-
-public class CreditsScreen extends JPanel {
-	CreditsScreen(HolderPanel holderPanel) {
-	
-	}
-}
